d3d167252823e223220fb519b0ee355f77fbe688
oisho105 <99174862+oisho105@users.noreply.github.com>
2022-02-20 12:50:04 +0600

    Create array_include.py

---
+++ array_include.py
@@ -0,0 +1,9 @@
+# Input no' in array
+from numpy import *
+
+# arr = linspace(1,50,50)  # 1st, last and parts
+arr2 = logspace(0,100,5)
+# arr3 = arange(1,15,2)       # put no. of steps
+
+# print(arr)
+print('%.2f' %arr2[2])
